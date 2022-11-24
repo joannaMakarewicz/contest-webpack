d5744a33960d6643f7ea81902c1f1a987e6eabc9
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-24 10:55:05 +0100

    html tags changing in main section and main picture added in head section

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
+import moment from 'moment';
+console.log(moment);
 
 const hamburger = document.querySelector('.hamburger--js');
 hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation--js');
     nav.classList.toggle('navigation__open');
 })
+
