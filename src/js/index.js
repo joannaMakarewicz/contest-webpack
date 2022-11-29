9ba3089d916290b00d8740613fc0bbb0f1266de4
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-29 16:10:38 +0100

    absolute icon for goting top - added

--- src/js/index.js
+++ src/js/index.js
@@ -4,39 +4,11 @@ import moment from "moment";
 console.log(moment);
 
 
-const article1 = document.querySelector('#question');
-const article2 = document.querySelector('#personal');
-
-const link1 = document.querySelector('.navigation__link-1--js');
-const link2 = document.querySelector('.navigation__link-2--js');
-
-console.log(article1.classList);
-
-window.addEventListener('scroll', () => {
-    if(article1.classList.contains('padding')){
-    article1.classList.remove('special__padding'); 
-    console.log('usunęłam');
-    }else{
-        console.log('nie ma klasy');
-    }
-})
-
-link1.addEventListener('click', () => {
-article1.classList.add('special__padding');
-console.log('dodałam');
-})
-
-link2.addEventListener('click', () => {
-    article2.classList.add('special__padding');
-})
-
-
+// navigation with hamburger
 
 const hamburger = document.querySelector(".hamburger--js");
 const nav = document.querySelector(".navigation--js");
 
-
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("navigation__open");
 });
-
