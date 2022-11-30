1e735ac3d142d9b2c3f99608a3da2f90c80ae9cd
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-30 13:23:13 +0100

    open menu color&size changed, JS for open menu

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,28 @@ console.log(moment);
 
 // navigation with hamburger
 
-const hamburger = document.querySelector(".hamburger--js");
 const nav = document.querySelector(".navigation--js");
+const navLinks = document.querySelectorAll('.navigation__link--js');
+const hamburger = document.querySelector(".hamburger--js");
+const closeIcon = document.querySelector('.hamburger__close--js');
+const menuIcon = document.querySelector('.hamburger__menu--js')
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("navigation__open");
-});
+function openMenu() {
+  if(nav.classList.contains("navigation__open")) {
+    nav.classList.remove("navigation__open");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  }else{
+    nav.classList.add("navigation__open");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", openMenu);
+
+navLinks.forEach(
+  function(menuLink) {
+    menuLink.addEventListener('click', openMenu);
+  }
+)
